faa4a144e3a9239608a4f7c9e273a30b2a6ba8fc
Denis Yevduschenko <a2ka7.dp@gmail.com>
2016-08-31 23:09:10 +0300

    Edited

--- src/main/java/PrintArray.java
+++ src/main/java/PrintArray.java
@@ -14,7 +14,7 @@ public class PrintArray {
 		System.out.println("Reverse Array:");
 		for (int i = 0; i < arr.length; i++){
 			for (int j = 0; j < arr.length; j++){
-				if (arr[i][j] == 0){
+				if (arr[i][j] == 0 && j < arr.length - i - 1){
 					System.out.print(" " + " ");
 					continue;
 				}
--- src/main/java/TimeParsier.java
+++ src/main/java/TimeParsier.java
@@ -7,42 +7,85 @@ import java.net.URLConnection;
 
 public class TimeParsier {
 	
-	public static void printTime(){
-		URL url = null;
-		URLConnection urlConn = null;
-		InputStreamReader inStream = null;
-		BufferedReader buff = null;
-		String time;
+	public static void main(String[] args) throws IOException {
+		TimeParsier timeParsier = new TimeParsier();
+		timeParsier.printTime();
+
+	}
+	
+	private URL url = null;
+	private URLConnection urlConn = null;
+	private InputStreamReader inStream = null;
+	private BufferedReader buff = null;
+	private String time;
+	
+	
+
+	public TimeParsier() throws IOException {
+		this.url = new URL("http://www.timeapi.org/utc/now");
+		this.urlConn = url.openConnection();
+		this.inStream = new InputStreamReader(urlConn.getInputStream());
+		this.buff = new BufferedReader(inStream);
+	}
+	
+	public void printTime(){
 		try {
-			url = new URL("http://www.timeapi.org/utc/now");
-			urlConn = url.openConnection();
-			inStream = new InputStreamReader(urlConn.getInputStream());
-			buff = new BufferedReader(inStream);
-			String temp = buff.readLine();
-			time = temp.substring(11, 19);
-			System.out.println("GMT: " + time); 
+			printTimeFromInternet();
 		} catch(MalformedURLException e) {
 			System.out.println("Please check the spelling of the URL: "
 							+ e.toString());
-		} catch (IOException e1) {
+		}catch (IOException e) {
 			System.out.println("Can't read from Internet: "
-					+ e1.toString());
-		} finally {
-			try {
-				inStream.close();
-				buff.close();
-			} catch(Exception e) {
-				System.out.println("Can't close streams "
-						+ e.getMessage());
-			}
+					+ e.toString());
+		}
+	}
+
+	private void printTimeFromInternet() throws IOException{
+		setTimeFromURL();
+		System.out.println("GMT: " + getTime()); 
+		closeAllConnections();
+	}
+
+	private void setTimeFromURL() throws IOException{
+		
+			String temp = buff.readLine();
+			time = temp.substring(11, 19);
+ 
+//		} catch(MalformedURLException e) {
+//			System.out.println("Please check the spelling of the URL: "
+//							+ e.toString());
+//		} catch (IOException e1) {
+//			System.out.println("Can't read from Internet: "
+//					+ e1.toString());
+//		} finally {
+//			try {
+//				inStream.close();
+//				buff.close();
+//			} catch(Exception e) {
+//				System.out.println("Can't close streams "
+//						+ e.getMessage());
+//			}
+//		}
+		
+	}
+	
+	private void closeAllConnections(){
+		try {
+			inStream.close();
+			buff.close();
+		} catch (IOException e) {
+			System.out.println("Can't read from Internet: "
+					+ e.toString());
 		}
 		
 	}
 	
-	public static void main(String[] args) {
-		
-		TimeParsier.printTime();
+	public String getTime() {
+		return time;
+	}
 
+	public void setTime(String time) {
+		this.time = time;
 	}
 
 }
